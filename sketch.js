dd7f47d5bcbb87f3cb0ac8915705b0cdb76c3dcb
leils <e.leiachang@gmail.com>
2019-09-28 15:30:19 -0700

    constructors

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var capture;
 var w = 640,
     h = 480;
 
+var elements = [];
 var detector;
 var classifier = objectdetect.frontalface;
 
@@ -9,6 +10,44 @@ var frameNum = 0;
 
 var faces = [];
 var faceCount = 0;
+
+class CircleStruct {
+  constructor(x, y) {
+    //     position
+    this.radius = 50;
+    this.x = x || random(this.radius, width - this.radius);
+    this.y = y || random(this.radius, height - this.radius);
+    this.xInc = 1;
+    this.yInc = 1;
+
+  }
+
+  // check if hitting the walls
+  bounceCheck() {
+    if (this.x > width - this.radius || this.x < this.radius) {
+      this.xInc *= -1;
+    }
+
+    if (this.y > height - this.radius || this.y < this.radius) {
+      this.yInc *= -1;
+    }
+  }
+
+  //movement
+  step() {
+    this.x += this.xInc;
+    this.y += this.yInc;
+
+    this.bounceCheck();
+  }
+
+  draw() {
+    stroke(255, 0, 0);
+    ellipse(this.x, this.y, this.radius, this.radius);
+    print(this.x, this.y);
+    this.step();
+  }
+}
 
 function setupDetector() {
     capture = createCapture({
@@ -35,12 +74,22 @@ function setup() {
   setupDetector();
   frameRate(24);
 
+  var newCircle = new CircleStruct;
+  elements.push(newCircle);
+
   setInterval(countFaces, 3000);
 }
 
 function draw() {
-  image(capture, 0, 0, w, h);
+  // image(capture, 0, 0, w, h);
   text(`faces visible: ${faceCount}`, 10, 20);
+
+
+  background(51);
+
+  elements.forEach(circ => {
+     circ.draw();
+  });
 }
 
 function countFaces() {
